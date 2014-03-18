c54fe823394f9fc83069f1c5cb38efbe3debde04
lucarin91 <lucarin91@gmail.com>
2014-03-18 12:05:09 +0100

    non funziona un cazzo, sistemalo te io mi vado a fumare una sigaretta

--- three.js/js/blenderImport.js
+++ three.js/js/blenderImport.js
@@ -1,19 +1,28 @@
-var loader = new THREE.JSONLoader();
+var loader;
+var scene1;
+//costruttorevar 
+function blenderImporter(scene){
+	scene1 = scene;
+	loader = new THREE.JSONLoader();
+}
 
-function blenderImport (fileName, scale, positionVector, scene){
+blenderImporter.prototype.import = function(fileName, scale, positionVector, rotationVector, callBack){
 	//console.log(scale);
 	//console.log(positionVector);
-	var mesh;
 	//var loader = new THREE.JSONLoader();
+	var object;
 	loader.load("http://127.0.0.1:8080/modelli/"+fileName, function( geometry, materials ) {
-		mesh = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial(materials));
-        mesh.scale.set( scale, scale, scale );
-        mesh.position.x = positionVector[0];
-        mesh.position.y = positionVector[1];
-        mesh.position.z = positionVector[2];
-        mesh.castShadow = true;
-        mesh.receiveShadow = true;
-        scene.add(mesh);
+		geometry.applyMatrix( new THREE.Matrix4().makeRotationX(rotationVector[0]));
+		geometry.applyMatrix( new THREE.Matrix4().makeRotationY(rotationVector[1]));
+		geometry.applyMatrix( new THREE.Matrix4().makeRotationZ(rotationVector[3]));
+		object = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial(materials));
+        object.scale.set( scale, scale, scale );
+        object.position.x = positionVector[0];
+        object.position.y = positionVector[1];
+        object.position.z = positionVector[2];
+        object.castShadow = true;
+        object.receiveShadow = true;
+        callBack(geometry, object);
+        scene1.add(object);
     }); 
-    
 }
