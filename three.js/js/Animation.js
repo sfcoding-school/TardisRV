c886a21e97124d606123f2afcc57d78ced01595c
lucarin91 <lucarin91@gmail.com>
2014-03-18 21:21:30 +0100

    sistemato bug animazione manopola

--- three.js/js/Animation.js
+++ three.js/js/Animation.js
@@ -7,40 +7,32 @@ var t = 0;
 function Animation(object){
 	
    	this.object = object;
-
-   	console.log(this.object);
-   
+   	//console.log(this.object);
 }
 
 // metodi
 Animation.prototype.update = function(){
-	//console.log(muoviManopola);
     if (muoviManopola){
-    	if (revert){
-    	    if (t<30){
+        if (revert){
+            if (t>=0){
                 this.object.rotateOnAxis( new THREE.Vector3(1,0,-0.70).normalize(), 0.05 );
-                t+=1;
+                t-=1;
             }else{
-                t=0;
                 muoviManopola=false;
                 revert = false;
             }
         }else{
-        	if (t<30){
+            if (t<=30){
                 this.object.rotateOnAxis( new THREE.Vector3(1,0,-0.70).normalize(), -0.05 );
                 t+=1;
             }else{
-                t=0;
                 muoviManopola=false;
             }
         }
-        console.log(t);
-        //t+=1;
 	}
 }
 
 Animation.prototype.startAni = function(){
-	console.log(muoviManopola);
 	muoviManopola = true;
 	if (up){
 		revert = false;
@@ -50,8 +42,4 @@ Animation.prototype.startAni = function(){
 		up = true;
 	}
 
-}
-
-Animation.prototype.revertAni = function(){
-	muoviManopola = true;
 }
