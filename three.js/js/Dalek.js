cd7a0c3d523f328cd5fba057f775955bc0b19ff1
Ulisse91 <fedo.coro@gmail.com>
2014-07-09 16:57:58 +0200

    inizio Dalek

---
+++ three.js/js/Dalek.js
@@ -0,0 +1,24 @@
+function dalekC(scene){
+	this.scene = scene;
+}
+
+dalekC.prototype.manage = function(){
+
+	var onKeyDown = function ( event ) {
+
+		switch ( event.keyCode ) {
+
+			case 72: // up
+				console.log("H");
+				break;
+		}
+
+	};
+
+	document.addEventListener( 'keydown', onKeyDown, false );
+
+}
+
+dalekC.prototype.update = function(){
+	
+}
