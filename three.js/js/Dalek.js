e7c29e962e50efe0e9f0124e9010c88934aaf90f
Ulisse91 <fedo.coro@gmail.com>
2014-07-10 14:32:15 +0200

    dalek

--- three.js/js/Dalek.js
+++ three.js/js/Dalek.js
@@ -1,55 +1,60 @@
 var vDalek = 0.01;
 var dio  = 0;
-var dio2 = Math.floor(Math.random() * 350) + 200;
-var dalek;
-var inMovimento;
+var dio2 = 0;
+var inMovimento = false;
+var rDalek = 0;
 
-function Dalek(scene, rDalek, dalek){
+function Dalek(scene, dalek){
 	this.scene = scene;
-	this.rDalek = rDalek;
 	this.dalek = dalek;
-	inMovimento = false;
 }
 
-Dalek.prototype.manage = function(dalek){
-
-	
-
-	var onKeyDown = function ( event ) {
-		console.log("click");
+Dalek.prototype.manage = function(){
+	var scene = this.scene;
+	var dalek = this.dalek;
+	var onKeyDown = function (event) {
 		switch ( event.keyCode ) {
-
 			case 72:
 				console.log("H");
 			 	if (!inMovimento) {
-					this.scene.add(dalek);
+			 		console.log(scene);
+			 		console.log("H1");
+					scene.add(dalek);
 					inMovimento = true;
+
+					dio  = Math.floor(Math.random() * 2*Math.PI);
+					dio2 = Math.floor(Math.random() * 350) + 200;
+
 				} else {
-					this.scene.remove(dalek);
+					console.log("H2");
+					scene.remove(dalek);
 					inMovimento = false;
 				}
 				break;
-	 	};
+		}
+	};
 
-		document.addEventListener( 'keydown', onKeyDown, false );
+	document.addEventListener( 'keydown', onKeyDown, false );
 
-	}
+	setInterval(function() {
+                rDalek = Math.floor(Math.random() * 450) + 180;
+            }, 10000);
 }
 
 Dalek.prototype.update = function(){
 	    if (inMovimento) {
 	    	dio += vDalek;
 
-            if (dio2 >= this.rDalek){
+            if (dio2 >= rDalek){
                 dio2-=vDalek+1;
             }
             else{
                 dio2+=vDalek+1;
             }
 
-            dalek.position.x = Math.sin(dio) * dio2;
-            dalek.position.z = Math.cos(dio) * dio2;
-            dalek.rotation.y += vDalek;
+            this.dalek.position.x = Math.sin(dio) * dio2;
+            this.dalek.position.z = Math.cos(dio) * dio2;
+            this.dalek.rotation.y += vDalek;
         }
 }
 
